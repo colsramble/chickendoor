5a2d16481bd3be129ce0e3c2de3072906ec39a94
Colin <colin@colsramble.com>
2017-01-12 20:56:26 +1030

    Removed unused code for NTP and added door status to response

--- firmware_esp8266/controller/controller.ino
+++ firmware_esp8266/controller/controller.ino
@@ -4,18 +4,6 @@
 #include <ESP8266mDNS.h>
 #include <FS.h>
 #include "config.h"
-#include <TimeLib.h>
-#include <WiFiUdp.h>
-
-// NTP Servers:
-static const char ntpServerName[] = "0.au.pool.ntp.org";
-const int timeZone = 0;   // GMT
-
-WiFiUDP Udp;
-unsigned int localPort = 8888;  // local port to listen for UDP packets
-
-time_t getNtpTime();
-void sendNTPpacket(IPAddress &address);
 
 /////////////////////
 // Pin Definitions //
@@ -29,9 +17,7 @@ void sendNTPpacket(IPAddress &address);
 #define isOpen() (digitalRead(PI_OPEN) == 0)
 #define isClosed() (digitalRead(PI_CLOSED) == 0)
 
-
 #define DBG_OUTPUT_PORT Serial
-
 
 const char WiFiAPPSK[] = WIFI_AP_SECRET;
 const char* ssid       = WIFI_SSID;
@@ -40,11 +26,11 @@ const char* host       = HOSTNAME;
 
 ESP8266WebServer server(SERVER_PORT);
 
-void openDoor() {
+int openDoor() {
   int timeout = 4000;
   
   if(isOpen())
-    return;
+    return 0;
 
   digitalWrite(PO_OPEN, 1);
   digitalWrite(PO_ENERGIZE, 1);
@@ -53,13 +39,15 @@ void openDoor() {
   }
   digitalWrite(PO_ENERGIZE, 0);
   digitalWrite(PO_OPEN, 0);  
+
+  return (timeout > 0 ? 0 : 1);
 }
 
-void closeDoor() {
+int closeDoor() {
   int timeout = 4000;
   
   if(isClosed())
-    return;
+    return 0;
 
   digitalWrite(PO_OPEN, 0);
   digitalWrite(PO_ENERGIZE, 1);
@@ -68,29 +56,39 @@ void closeDoor() {
   }
   digitalWrite(PO_ENERGIZE, 0);
   digitalWrite(PO_OPEN, 0);  
+
+  return (timeout > 0 ? 0 : 1);
 }
 
 void handleOpen() {
   DBG_OUTPUT_PORT.println("Open Door....");
   openDoor();
   DBG_OUTPUT_PORT.println("Done.");
-  server.send(200, "text/json", "OK");
+  handleGetStatus();
 }
 
 void handleClose() {
   DBG_OUTPUT_PORT.println("Close Door....");
   closeDoor();
   DBG_OUTPUT_PORT.println("Done.");
-  server.send(200, "text/json", "OK");
+  handleGetStatus();
 }
 
 void handleGetLightLevel() {
   DBG_OUTPUT_PORT.println("handleGetLightLevel");
-  String output = "{ \"level\": ";
+  handleGetStatus();
+}
+
+void handleGetStatus() {
+  DBG_OUTPUT_PORT.println("handleGetStatus");
+  String output = "{ \"lightlevel\": ";
   output += getLightLevel();
+  output += ", \"door\": ";
+  output += (isOpen() ? "\"open\"" : (isClosed() ? "\"closed\"" : "unknown") ); 
   output += " }";
   server.send(200, "text/json", output);
 }
+
 
 void setupWiFi()
 {
@@ -118,6 +116,7 @@ void setupWiFi()
   server.on("/open",   HTTP_GET, handleOpen);
   server.on("/close",  HTTP_GET, handleClose);
   server.on("/level",  HTTP_GET, handleGetLightLevel);
+  server.on("/stat",   HTTP_GET, handleGetStatus);
   server.onNotFound([](){
       server.send(404, "text/plain", "FileNotFound");
   });
@@ -126,16 +125,7 @@ void setupWiFi()
 
   Serial.print("IP number assigned by DHCP is ");
   Serial.println(WiFi.localIP());
-  Serial.println("Starting UDP");
-  Udp.begin(localPort);
-  Serial.print("Local port: ");
-  Serial.println(Udp.localPort());
-  Serial.println("waiting for sync");
-  setSyncProvider(getNtpTime);
-  setSyncInterval(1800);
-
 }
-
 
 void initHardware()
 {
@@ -160,64 +150,6 @@ void loop()
   server.handleClient();
 }
 
-/*-------- NTP code ----------*/
-
-const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
-
-time_t getNtpTime()
-{
-  IPAddress ntpServerIP; // NTP server's ip address
-
-  while (Udp.parsePacket() > 0) ; // discard any previously received packets
-  Serial.println("Transmit NTP Request");
-  // get a random server from the pool
-  WiFi.hostByName(ntpServerName, ntpServerIP);
-  Serial.print(ntpServerName);
-  Serial.print(": ");
-  Serial.println(ntpServerIP);
-  sendNTPpacket(ntpServerIP);
-  uint32_t beginWait = millis();
-  while (millis() - beginWait < 1500) {
-    int size = Udp.parsePacket();
-    if (size >= NTP_PACKET_SIZE) {
-      Serial.println("Receive NTP Response");
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
-      unsigned long secsSince1900;
-      // convert four bytes starting at location 40 to a long integer
-      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
-      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
-      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
-      secsSince1900 |= (unsigned long)packetBuffer[43];
-      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
-    }
-  }
-  Serial.println("No NTP Response :-(");
-  return 0; // return 0 if unable to get the time
-}
-
-// send an NTP request to the time server at the given address
-void sendNTPpacket(IPAddress &address)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12] = 49;
-  packetBuffer[13] = 0x4E;
-  packetBuffer[14] = 49;
-  packetBuffer[15] = 52;
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
-}
 
 
 
